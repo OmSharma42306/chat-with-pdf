570cc515805d4b47278567e5d309a805604029e8
Om Sharma <omsharma.83173@gmail.com>
2025-07-18 15:00:24 +0530

    feat : added-chunking-embedding-per-chunk-and-searching-chunk-on-user-query.

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,12 +4,24 @@ dotnev.config();
 import cors from "cors";
 import multer from "multer";
 import { Queue } from "bullmq";
+import { GoogleGenAI } from "@google/genai";
+import { QdrantClient } from '@qdrant/js-client-rest';
+
+
 
 const app = express();
 const PORT = 9000;
 app.use(cors());
 
 
+// Embedding Model Client
+const ai = new GoogleGenAI({apiKey:process.env.GEMINI_API_KEY});
+
+// Qdrant Client 
+const qdrantClient = new QdrantClient({
+    url : process.env.QDRANT_CLIENT_URL,
+    apiKey : process.env.QDRANT_API_KEY,
+});
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -35,7 +47,7 @@ app.post('/upload-file',upload.single('file'),async (req:Request,res:Response)=>
         await myQueue.add(`${file?.originalname}`,
             {name : file?.originalname,
             path : file?.path,
-            destination : file?.destination
+            destination : file?.destination,
         });
         res.status(200).json({msg: "PDF Processing!"});
         return;
@@ -43,6 +55,42 @@ app.post('/upload-file',upload.single('file'),async (req:Request,res:Response)=>
         res.status(400).json({error});
         return;
     }
+});
+
+
+app.get('/chat-pdf',async (req:Request,res:Response)=>{
+    // const userMessage = req.query.message as string;
+    const userMessage = " what is Squad Balance?"
+
+    if(!userMessage){
+        res.status(400).json({msg:"User Query Not Found!"});
+        return;
+    }
+    // embed the user query.
+    const response = await ai.models.embedContent({
+        model: 'gemini-embedding-001',
+        contents: `${userMessage}`,
+    });
+
+    // user query embeddings.
+    const queryEmbeddings : any = response!.embeddings![0].values;
+
+    // search the queryembedding in qdrant db.
+    const searchResults = await qdrantClient.search("test_collection3",{
+        vector:queryEmbeddings,
+        limit:3,
+        with_payload:true,
+    });
+
+    console.log("Search Results : ",searchResults);
+    
+    const matchedChunks = searchResults.map((r:any) => r.payload?.fileData?.pageContent).filter(Boolean)
+    
+    console.log(matchedChunks);
+    res.json({matchedChunks});
+   
+
+
 })
 
 
--- server/src/worker.ts
+++ server/src/worker.ts
@@ -1,77 +1,91 @@
-import { Worker } from 'bullmq';
-import IORedis from "ioredis"
-import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf"
-import { QdrantClient } from '@qdrant/js-client-rest';
+import { Worker } from "bullmq";
+import IORedis from "ioredis";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { QdrantClient } from "@qdrant/js-client-rest";
 import { GoogleGenAI } from "@google/genai";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 // Redis Connection
-const connection = new IORedis({maxRetriesPerRequest:null});
+const connection = new IORedis({ maxRetriesPerRequest: null });
 
 // Embedding Model Client
-const ai = new GoogleGenAI({apiKey:process.env.GEMINI_API_KEY});
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
-
-// Qdrant Client 
+// Qdrant Client
 const qdrantClient = new QdrantClient({
-    url : process.env.QDRANT_CLIENT_URL,
-    apiKey : process.env.QDRANT_API_KEY,
+  url: process.env.QDRANT_CLIENT_URL,
+  apiKey: process.env.QDRANT_API_KEY,
 });
 
 const worker = new Worker(
-    'pdf-queue',
-    async job =>{
-        console.log(job.data);
-        // get the path of pdf 
-        const path = job.data.path;
-        console.log("Path : ",path);
-        // retrive data
-        const loader = new PDFLoader(path);
-        
-        // load the document
-        const docs = await loader.load();
-        console.log("DOCS",docs[0]);
+  "pdf-queue",
+  async (job) => {
+    console.log(job.data);
+    // get the path of pdf
+    const path = job.data.path;
+    console.log("Path : ", path);
+    // retrive data
+    const loader = new PDFLoader(path);
 
-        // chunk the pdf data
-        // convert that chunk into vector embeddings.
-        
-       const response = await ai.models.embedContent({
-        model: 'gemini-embedding-001',
-        contents: `${docs[0]}`,
-    });
-    // console.log(response.embeddings);
-        // upload it into qdrant db.
-        if(response.embeddings){
-            const embeddings = response.embeddings[0].values
-            console.log("FINAL EMBEDDINGS : ",embeddings);
-            console.log("Embedding length:", embeddings!.length); 
+    // load the document
+    const docs = await loader.load();
+    // console.log("DOCS",docs);
 
-            try{
-                // if you want to create collection call this function : createCollectionForQdrantDB();
-                const operationInfo = await qdrantClient.upsert("test_collection3",{
-                    wait:true,
-                    points:[ { id: Number(job.id), vector: embeddings!, payload: { fileName: job.data.name } },]
-                });
-                
-                console.log("Operation INFO : ",operationInfo);
-            
-            }catch(error){
-                console.log(error);
-            }
+    // chunk the pdf data
+
+    const points: any = [];
+    for (let i = 0; i < docs.length; i++) {
+      const content = docs[i].pageContent;
+
+      // convert that per chunk into vector embeddings.
+
+      const response = await ai.models.embedContent({
+        model: "gemini-embedding-001",
+        contents: `${content}`,
+      });
+
+      if (response.embeddings) {
+        const vector = response.embeddings[0].values;
+        console.log("FINAL EMBEDDINGS : ", vector);
+        console.log("Embedding length:", vector!.length);
+
+        try {
+          points.push({
+            id: Number(`${job.id}${i}`),
+            vector,
+            payload: {
+              fileName: job.data.name,
+              fileData: {
+                pageContent: content,
+              },
+            },
+          });
+        } catch (error) {
+          console.log(error);
         }
-    },
-    {connection}
+      }
+    }
+    // if you want to create collection call this function : createCollectionForQdrantDB();
+    // await createCollectionForQdrantDB() ;
+    const operationInfo = await qdrantClient.upsert("test_collection3", {
+      wait: true,
+      points: points,
+    });
+
+    console.log("Operation INFO : ", operationInfo);
+    console.log("Upserted Points:", points.length);
+  },
+  { connection }
 );
 
+worker.on("completed", (job) => {
+  console.log(`${job.id} has completed.`);
+});
 
-worker.on('completed',job =>{
-    console.log(`${job.id} has completed.`);
-}); 
-
-
-async function createCollectionForQdrantDB(){
-    await qdrantClient.createCollection("test_collection3",{
-        vectors:{size:3072,distance:"Dot"},
-    })
+async function createCollectionForQdrantDB() {
+  await qdrantClient.createCollection("test_collection3", {
+    vectors: { size: 3072, distance: "Dot" },
+  });
 }
